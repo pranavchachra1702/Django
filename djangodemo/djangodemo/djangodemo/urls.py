23457feb7b6cfedf43fea5b54f72525a3e79cf25
pranavchachra1702 <pranavchachra1702@gmail.com>
2025-09-01 11:12:06 +0530

    Add static pages with forms and media embeds
    - Added index.html for Home page
    - Added about.html with paragraph and embedded YouTube video
    - Added services.html for services section
    - Added register.html with registration form
    - Added contact.html with contact form and Google Map embed

--- djangodemo/djangodemo/djangodemo/urls.py
+++ djangodemo/djangodemo/djangodemo/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("services/",views.services, name="services"),
+    path("contact/",views.contact, name="contact"),
+    path("register/",views.register, name="register")
 ]
--- djangodemo/djangodemo/mysite/views.py
+++ djangodemo/djangodemo/mysite/views.py
@@ -11,3 +11,12 @@ def about(request):
     # return HttpResponse("<h1 style='color:green;'>Welcome to About Us Page</h1>")
     # HttpResponse() to display a message render() to display an HTML
     return render(request, 'about.html', {})
+
+def services(request):
+    return render(request, 'services.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+def register(request):
+    return render(request, 'register.html', {})
